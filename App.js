b38fadcf1630f7fb56b959592647311ba66a8141
yuudachi13 <t2pupa00@students.oamk.fi>
2024-08-31 03:19:10 +0300

    test

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import DateTimePicker from 'react-native-ui-datepicker';
+import dayjs from 'dayjs'
 
 export default function App() {
+
+  const [date, setDate] = useState(dayjs())
+  const [show, setShow] = useState(false)
+
+  const pickDate = () => {
+    setShow(true)
+  }
+  const selectDate = (value) => {
+    setDate(value)
+    setShow(false)
+  }
+  
+  const DisplayDate  = () => {
+    return (
+      <Text>{dayjs(date).locale('fi').format('DD.MM.YYYY')} </Text>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable onPress={pickDate}> 
+        <DisplayDate  />
+        </Pressable>
+        {show &&
+      <DateTimePicker
+      date={date}
+      onChange={(tesiti) => selectDate(tesiti.date)}
+      />
+      }
     </View>
   );
 }
